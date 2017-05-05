731469f681b1c9024c392e315e6946a2ae197afb
Abhishek Thapliyal <abhishek@nickelfox.com>
2017-05-05 15:04:05 +0530

    #1

--- NewReader/iWatchNews Extension/NewsRowController.swift
+++ NewReader/iWatchNews Extension/NewsRowController.swift
@@ -11,18 +11,28 @@ import WatchKit
 
 class NewsRowController: NSObject {
 
-    
     @IBOutlet var imgView: WKInterfaceImage?
-    
     @IBOutlet var textLabel: WKInterfaceLabel?
+    @IBOutlet var descLabel: WKInterfaceLabel!
+    
     
     var newsObject: NRSource? {
     
         didSet{
             if let source = newsObject {
                 self.imgView?.setImage(UIImage(named: "download.jpeg"))
-                self.textLabel?.setText(source.sourceDescription! as String)
+                self.textLabel?.setText(source.name! as String)
+                self.descLabel?.setText(source.sourceDescription! as String)
             }
         }
     }
 }
+
+
+/*
+ REMEMBER ROW LAYOUT HORIZONTAL OR VERTICAL FOR ALIGNMENT OF SUBVIEWS IN GROUP
+ SUPER GROUP -> SUB GROUP & LABEL
+ SUPER GROUP = VERTICAL LAYOUT
+ SUB GROUP = HORIZONTAL LAYOUT
+ 
+ */
