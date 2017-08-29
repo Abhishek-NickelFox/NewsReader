40494cd4f7eff60449824a4163ae67d5d4c5e919
Abhishek Thapliyal <abhishek@Apples-iMac.local>
2017-08-29 14:10:53 +0530

    ##

---
+++ NewReader/NewReader/NRAPIClient.swift
@@ -0,0 +1,14 @@
+//
+//  NRAPIClient.swift
+//  NewReader
+//
+//  Created by Abhishek on 29/08/17.
+//  Copyright © 2017 Abhishek Thapliyal. All rights reserved.
+//
+
+import UIKit
+import APIClient
+
+class NRAPIClient: NSObject {
+
+}
